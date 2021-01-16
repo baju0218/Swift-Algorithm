61b96d13532e565baa1d0e6b1e09a5080b8175b0
baju0218 <baju0218@naver.com>
2021-01-16 21:49:27 +0900

    Programmers Level 1 1~20

--- solution/solution/main.swift
+++ solution/solution/main.swift
@@ -1,8 +1,5 @@
 import Foundation
 
-func solution(_ a:Int, _ b:Int) -> Int64 {
-    let sum = a + b
-    let cnt = abs(a - b) + 1
-    
-    return Int64(sum * cnt / 2)
-}
+var temp: String? = nil
+
+print(temp)
